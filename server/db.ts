92895c60c1452711e195083691794b61311659d6
Peter Mazza <petermazza@Peters-MacBook-Air.local>
2026-02-14 21:58:23 -0500

    Defer DATABASE_URL check to show better error

--- server/db.ts
+++ server/db.ts
@@ -4,11 +4,15 @@ import * as schema from "@shared/schema";
 
 const { Pool } = pg;
 
-if (!process.env.DATABASE_URL) {
-  throw new Error(
-    "DATABASE_URL must be set. Did you forget to provision a database?",
-  );
+function getDatabaseUrl() {
+  const url = process.env.DATABASE_URL;
+  if (!url) {
+    throw new Error(
+      "DATABASE_URL must be set. Did you forget to provision a database?",
+    );
+  }
+  return url;
 }
 
-export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+export const pool = new Pool({ connectionString: getDatabaseUrl() });
 export const db = drizzle(pool, { schema });
